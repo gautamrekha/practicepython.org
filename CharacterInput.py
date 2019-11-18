da2bae9b9a2e5be5f303eb16d855f1b6d83e5320
rekhagautam <rekhagautam1992@gmail.com>
2019-11-18 15:40:34 +0800

    Add files via upload

---
+++ CharacterInput.py
@@ -0,0 +1,5 @@
+name = input("Hi there! what's your name? \n")
+age = input("can you share your age with me please? \n")
+years_to_go = 100 - int(age)
+print("Hello "+ name +"! " + "You'll turn 100 in " +
+str(years_to_go) +" years" )
